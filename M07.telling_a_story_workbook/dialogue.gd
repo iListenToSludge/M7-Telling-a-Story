b11c3d9e422945ee7e95427432033c026bebf8ba
ZACKBOY <sevang@s.sfusd.edu>
2025-10-31 15:20:13 -0700

    finished m7 l5

---
+++ M07.telling_a_story_workbook/dialogue.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = $NextButton
+var dialogue_items: Array[String] = [
+	" 3 hawaiian drive-in burgers " ,
+	 " and 1 burger special " ,
+	 " and 1 riceplate with short ribs " ,
+]
+
+var current_item_index:= 0 
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+	
+func advance() -> void: 
+	current_item_index+= 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
+
+func _ready() -> void: 
+	show_text()
+	next_button.pressed.connect(advance)
