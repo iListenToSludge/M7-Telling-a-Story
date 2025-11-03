65b95c75d977ed097d2563977daf5dcd0c4086df
ZACKBOY <sevang@s.sfusd.edu>
2025-11-03 15:40:04 -0800

    finished m7 l7

---
+++ M07.telling_a_story_workbook/expressions.gd
@@ -0,0 +1,7 @@
+extends Control
+@onready var body: TextureRect = %Body
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
